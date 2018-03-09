4331b82eee885729055ce6de84b8d8635a1e92b9
benjobs <benjobs@qq.com>
2018-03-09 19:33:32 +0800

    fixbug!

--- opencron-server/src/main/java/org/opencron/server/bootstrap/Startup.java
+++ opencron-server/src/main/java/org/opencron/server/bootstrap/Startup.java
@@ -18,6 +18,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
+
 package org.opencron.server.bootstrap;
 
 import static org.opencron.common.Constants.LauncherType;
