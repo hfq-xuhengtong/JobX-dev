6471edf8a7cc6fdb687322e20564f74537b17c92
benjobs <benjobs@qq.com>
2018-03-12 18:41:10 +0800

    更改序列化模块为阿里dubbo的序列化实现

--- opencron-common/src/main/java/org/opencron/common/serialize/fastjson/FastjsonSerializer.java
+++ opencron-common/src/main/java/org/opencron/common/serialize/fastjson/FastjsonSerializer.java
@@ -1,28 +1,52 @@
+/**
+ * Copyright (c) 2015 The Opencron Project
+ * <p>
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package org.opencron.common.serialize.fastjson;
 
-import com.alibaba.fastjson.JSON;
+import org.opencron.common.serialize.ObjectInput;
+import org.opencron.common.serialize.ObjectOutput;
 import org.opencron.common.serialize.Serializer;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 /**
- *
+ * FastJsonSerialization
  */
+public class FastJsonSerializer implements Serializer {
 
-public class FastjsonSerializer implements Serializer {
-
-    private static final String CHARSET = "UTF-8";
-
-    @Override
-    public byte[] encode(Object msg) throws IOException {
-        String jsonString = JSON.toJSONString(msg);
-        return jsonString.getBytes(CHARSET);
+    public byte getContentTypeId() {
+        return 6;
     }
 
-    @Override
-    public <T> T decode(byte[] buf, Class<T> type) throws IOException {
-        String jsonString = new String(buf, CHARSET);
-        return JSON.parseObject(jsonString, type);
+    public String getContentType() {
+        return "text/json";
+    }
+
+    public ObjectOutput serialize(OutputStream output) throws IOException {
+        return new FastJsonObjectOutput(output);
+    }
+
+    public ObjectInput deserialize(InputStream input) throws IOException {
+        return new FastJsonObjectInput(input);
     }
 
 }
