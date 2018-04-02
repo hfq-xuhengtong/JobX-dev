16894b9bd0f63a0aa6fe98b50b0427db3f9c3c37
benjobs <benjobs@qq.com>
2018-04-02 08:51:08 +0800

    build support!

--- build.sh
+++ build.sh
@@ -1,4 +1,24 @@
 #!/bin/bash
+#
+# Copyright (c) 2015 The Opencron Project
+# <p>
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements. See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership. The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License. You may obtain a copy of the License at
+# <p>
+# http://www.apache.org/licenses/LICENSE-2.0
+# <p>
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied. See the License for the
+# specific language governing permissions and limitations
+# under the License.
+#
 
 #echo color
 WHITE_COLOR="\E[1;37m";
@@ -43,6 +63,40 @@ echo_w () {
     [ $# -ne 1 ] && return 1
     printf "[${BLUE_COLOR}opencron${RES}] ${WHITE_COLOR}$1${RES}\n"
 }
+
+# OS specific support.  $var _must_ be set to either true or false.
+cygwin=false
+darwin=false
+os400=false
+case "`uname`" in
+CYGWIN*) cygwin=true;;
+Darwin*) darwin=true;;
+OS400*) os400=true;;
+esac
+
+# resolve links - $0 may be a softlink
+PRG="$0"
+
+while [ -h "$PRG" ]; do
+  ls=`ls -ld "$PRG"`
+  link=`expr "$ls" : '.*-> \(.*\)$'`
+  if expr "$link" : '/.*' > /dev/null; then
+    PRG="$link"
+  else
+    PRG=`dirname "$PRG"`/"$link"
+  fi
+done
+
+# Get standard environment variables
+##############################################
+PRGDIR=`dirname "$PRG"`
+
+WORKDIR=`cd "$PRGDIR" >/dev/null; pwd`;
+
+OPENCRON_VERSION="1.2.0-RELEASE";
+
+DIST_HOME="${WORKDIR}/dist"
+##############################################
 
 # Make sure prerequisite environment variables are set
 if [ -z "$JAVA_HOME" -a -z "$JRE_HOME" ]; then
@@ -106,24 +160,21 @@ if [ -z "$JAVA_ENDORSED_DIRS" ]; then
   JAVA_ENDORSED_DIRS="$OPENCRON_HOME"/endorsed
 fi
 
-# Set standard commands for invoking Java, if not already set.
-if [ -z "$RUNJAVA" ]; then
-  RUNJAVA="$JRE_HOME"/bin/java
-fi
-if [ "$os400" != "true" ]; then
-  if [ -z "$_RUNJDB" ]; then
-    _RUNJDB="$JAVA_HOME"/bin/jdb
+if [ -z "$JAVACMD" ] ; then
+  if [ -n "$JAVA_HOME"  ] ; then
+    if [ -x "$JAVA_HOME/jre/sh/java" ] ; then
+      # IBM's JDK on AIX uses strange locations for the executables
+      JAVACMD="$JAVA_HOME/jre/sh/java"
+    else
+      JAVACMD="$JAVA_HOME/bin/java"
+    fi
+  else
+    JAVACMD="`which java`"
   fi
 fi
 
-if [ -z "$JAVA_HOME" -a -z "$JRE_HOME" ]; then
-    echo_r "Neither the JAVA_HOME nor the JRE_HOME environment variable is defined"
-    echo_r "At least one of these environment variable is needed to run this program"
-    exit 1
-fi
-
 #check java exists.
-$RUNJAVA >/dev/null 2>&1
+$JAVACMD >/dev/null 2>&1
 
 if [ $? -ne 1 ];then
   echo_r "ERROR: java is not install,please install java first!"
@@ -131,98 +182,95 @@ if [ $? -ne 1 ];then
 fi
 
 #check openjdk
-if [ "`${RUNJAVA} -version 2>&1 | head -1|grep "openjdk"|wc -l`"x == "1"x ]; then
+if [ "`$JAVACMD -version 2>&1 | head -1|grep "openjdk"|wc -l`"x == "1"x ]; then
   echo_r "ERROR: please uninstall OpenJDK and install jdk first"
   exit 1;
 fi
 
-# OS specific support.  $var _must_ be set to either true or false.
-cygwin=false
-darwin=false
-os400=false
-case "`uname`" in
-CYGWIN*) cygwin=true;;
-Darwin*) darwin=true;;
-OS400*) os400=true;;
-esac
+if [ -z "$M2_HOME" ] ; then
 
-# resolve links - $0 may be a softlink
-PRG="$0"
+  saveddir=`pwd`
 
-while [ -h "$PRG" ]; do
-  ls=`ls -ld "$PRG"`
-  link=`expr "$ls" : '.*-> \(.*\)$'`
-  if expr "$link" : '/.*' > /dev/null; then
-    PRG="$link"
-  else
-    PRG=`dirname "$PRG"`/"$link"
-  fi
-done
+  M2_HOME=`dirname "$PRG"`/..
 
+  # make it fully qualified
+  M2_HOME=`cd "$M2_HOME" && pwd`
 
-# Get standard environment variables
-PRGDIR=`dirname "$PRG"`
-
-WORKDIR=`cd "$PRGDIR" >/dev/null; pwd`;
-
-MAVEN_URL="http://mirror.bit.edu.cn/apache/maven/maven-3/3.5.2/binaries/apache-maven-3.5.2-bin.tar.gz";
-
-MAVEN_NAME="apache-maven-3.5.2-bin"
-
-UNPKG_MAVEN_NAME="apache-maven-3.5.2";
-
-OPENCRON_VERSION="1.2.0-RELEASE";
-
-MAVEN_PATH="/tmp";
-
-[ ! -d "$MAVEN_PATH" ] && mkdir $MAVEN_PATH;
-
-DIST_HOME="${WORKDIR}/dist"
-
-USER="`id -un`"
-LOGNAME="$USER"
-if [ $UID -ne 0 ]; then
-    echo_y "WARNING: Running as a non-root user, \"$LOGNAME\". Functionality may be unavailable. Only root can use some commands or options"
+  cd "$saveddir"
+  # echo Using m2 at $M2_HOME
 fi
 
-#check maven exists
-mvn >/dev/null 2>&1
+# For Cygwin, ensure paths are in UNIX format before anything is touched
+if $cygwin ; then
+  [ -n "$M2_HOME" ] &&
+    M2_HOME=`cygpath --unix "$M2_HOME"`
+  [ -n "$JAVA_HOME" ] &&
+    JAVA_HOME=`cygpath --unix "$JAVA_HOME"`
+  [ -n "$CLASSPATH" ] &&
+    CLASSPATH=`cygpath --path --unix "$CLASSPATH"`
+fi
 
-if [ $? -ne 1 ]; then
+# For Migwn, ensure paths are in UNIX format before anything is touched
+if $mingw ; then
+  [ -n "$M2_HOME" ] &&
+    M2_HOME="`(cd "$M2_HOME"; pwd)`"
+  [ -n "$JAVA_HOME" ] &&
+    JAVA_HOME="`(cd "$JAVA_HOME"; pwd)`"
+  # TODO classpath?
+fi
 
-    echo_y "WARNING:maven is not install!"
 
-    if [ -x "/tmp/${UNPKG_MAVEN_NAME}" ] ; then
-        echo_w "maven is already download,now config setting...";
-        MVN=/tmp/${UNPKG_MAVEN_NAME}/bin/mvn
-    else
-        echo_w "download maven Starting..."
-        echo_w "checking network connectivity ... "
-        net_check_ip=114.114.114.114
-        ping_count=2
-        ping -c ${ping_count} ${net_check_ip} >/dev/null
-        retval=$?
-        if [ ${retval} -eq 0 ] ; then
-            echo_w "network is connectioned,download maven Starting... "
-            wget -P "/tmp" $MAVEN_URL && {
-                echo_g "download maven successful!";
-                echo_w "install maven Starting"
-                tar -xzvf /tmp/${MAVEN_NAME}.tar.gz -C /tmp
-                MVN=/tmp/${UNPKG_MAVEN_NAME}/bin/mvn
-            }
-        elif [ ${retval} -ne 0 ]; then
-            echo_r "ERROR:network is blocked! download maven failed,please check your network!build error! bye!"
-            exit 1
-        fi
+CLASSWORLDS_LAUNCHER=org.codehaus.plexus.classworlds.launcher.Launcher
+
+# For Cygwin, switch paths to Windows format before running java
+if $cygwin; then
+  [ -n "$M2_HOME" ] &&
+    M2_HOME=`cygpath --path --windows "$M2_HOME"`
+  [ -n "$JAVA_HOME" ] &&
+    JAVA_HOME=`cygpath --path --windows "$JAVA_HOME"`
+  [ -n "$CLASSPATH" ] &&
+    CLASSPATH=`cygpath --path --windows "$CLASSPATH"`
+fi
+
+# traverses directory structure from process work directory to filesystem root
+# first directory with .mvn subdirectory is considered project base directory
+find_maven_basedir() {
+  local basedir=$(pwd)
+  local wdir=$(pwd)
+  while [ "$wdir" != '/' ] ; do
+    if [ -d "$wdir"/.mvn ] ; then
+      basedir=$wdir
+      break
     fi
+    wdir=$(cd "$wdir/.."; pwd)
+  done
+  echo "${basedir}"
+}
 
-elif [ "$MVN"x = ""x ]; then
-    MVN="mvn";
-fi
+# concatenates all lines of a file
+concat_lines() {
+  if [ -f "$1" ]; then
+    echo "$(tr -s '\n' ' ' < "$1")"
+  fi
+}
+
+export MAVEN_PROJECTBASEDIR=${MAVEN_BASEDIR:-$(find_maven_basedir)}
+MAVEN_OPTS="$(concat_lines "$MAVEN_PROJECTBASEDIR/.mvn/jvm.config") $MAVEN_OPTS"
+
+# Provide a "standardized" way to retrieve the CLI args that will
+# work with both Windows and non-Windows executions.
+MAVEN_CMD_LINE_ARGS="$MAVEN_CONFIG $@"
+export MAVEN_CMD_LINE_ARGS
+
+WRAPPER_LAUNCHER=org.apache.maven.wrapper.MavenWrapperMain
 
 echo_w "build opencron Starting...";
 
-$MVN clean install -Dmaven.test.skip=true;
+exec "$JAVACMD" \
+  $MAVEN_OPTS \
+  -classpath "$MAVEN_PROJECTBASEDIR/.mvn/wrapper/maven-wrapper.jar" \
+  "-Dmaven.home=${M2_HOME}" "-Dmaven.multiModuleProjectDirectory=${MAVEN_PROJECTBASEDIR}" \
+  ${WRAPPER_LAUNCHER} "$@"
 
 retval=$?
 
