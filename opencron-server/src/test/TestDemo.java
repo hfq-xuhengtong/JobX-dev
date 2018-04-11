5f316176e13fb71561246e445a09e11942b70514
benjobs <benjobs@qq.com>
2018-04-11 17:41:41 +0800

    fixbug！

--- opencron-server/src/test/TestDemo.java
+++ opencron-server/src/test/TestDemo.java
@@ -8,7 +8,7 @@ public class TestDemo {
     @Test
     public void test1() throws Exception {
 
-        String str = "tar -xzvf $1 /$1 >> ff.log";
+        String str = "tar -xzvf $1 /$1>> ff.log";
         //(?is)(?!true|false).
         System.out.println(str.replaceAll("[^\\s+]+\\$1|\\$1"," agent "));
 
